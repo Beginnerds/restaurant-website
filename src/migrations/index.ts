99d88ddbab120c49da5e0ad0114a617af2ef597f
Shubham <shubhamkalra619@gmail.com>
2025-04-21 20:20:26 +0530

    removed migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250421_113457 from './20250421_113457';
-
-export const migrations = [
-  {
-    up: migration_20250421_113457.up,
-    down: migration_20250421_113457.down,
-    name: '20250421_113457'
-  },
-];
